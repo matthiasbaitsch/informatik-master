57c3a88368cf29a7ba2f5109989e6b64cc732509
Matthias Baitsch <matthias.baitsch@me.com>
2026-05-04 23:12:40 +0200

    Folien Objekte

---
+++ bausteine/04-objekte/folien/projekt/01-value-types/ValueTypes.cs
@@ -0,0 +1,5 @@
+﻿int x = 111;
+int y = x;
+
+x = 222;
+Console.WriteLine($"x = {x} und y = {y}");
---
+++ bausteine/04-objekte/folien/projekt/02-reference-types-1/ReferenceTypes.cs
@@ -0,0 +1,13 @@
+﻿using BoDraw;
+
+Circle c = new Circle(0, 0, 1);
+c.FillColor = Colors.LightGoldenrodYellow;
+Rectangle r1 = new Rectangle(1.1, -1, 3.1, 1);
+r1.FillColor = Colors.HotPink;
+Rectangle r2 = r1;
+r1.Move(2.2, 0);
+r2.FillColor = Colors.SteelBlue;
+
+BoDrawApp app = new BoDrawApp();
+app.Add(c, r1, r2);
+app.Show();
