542c29c8852ece45f4cfc2abac18be91bdfb51b6
Matthias Baitsch <matthias.baitsch@me.com>
2026-05-05 10:34:40 +0200

    WIP

--- bausteine/04-objekte/folien/projekt/02-referenzvariablen-kopie/ReferenceTypesCopy.cs
+++ bausteine/04-objekte/folien/projekt/02-referenzvariablen-kopie/ReferenceTypesCopy.cs
@@ -2,10 +2,11 @@
 
 Circle c = new Circle(0, 0, 1);
 c.FillColor = Colors.LightGoldenrodYellow;
+
 Rectangle r1 = new Rectangle(1.1, -1, 3.1, 1);
 r1.FillColor = Colors.HotPink;
-Rectangle r2 = r1;
-r1.Move(2.2, 0);
+
+Rectangle r2 = r1.Copy(2.2, 0);
 r2.FillColor = Colors.SteelBlue;
 
 BoDrawApp app = new BoDrawApp();
---
+++ bausteine/04-objekte/folien/projekt/04-exemplare/Instances.cs
@@ -0,0 +1,14 @@
+﻿using BoDraw;
+
+Rectangle r1 = new Rectangle(0, 0, 1, 1);
+r1.FillColor = Colors.HotPink;
+Rectangle r2 = new Rectangle(1, 0, 2, 1);
+r2.FillColor = Colors.Orange;
+Rectangle r3 = new Rectangle(0, 1, 1, 2);
+r3.FillColor = Colors.PaleVioletRed;
+Rectangle r4 = new Rectangle(1, 1, 2, 2);
+r4.FillColor = Colors.DarkRed;
+
+BoDrawApp app = new BoDrawApp();
+app.Add(r1, r2, r3, r4);
+app.Show();
