26af5d5bd7a1125eaeaa7a2bfb483b88a9c0a13e
Matthias Baitsch <matthias.baitsch@me.com>
2026-05-08 16:48:46 +0200

    Format mit # ergänzt

--- bausteine/03-elementare-datentypen/folien/projekt/03-ausgabe/Ausgabe.cs
+++ bausteine/03-elementare-datentypen/folien/projekt/03-ausgabe/Ausgabe.cs
@@ -18,15 +18,23 @@ Console.WriteLine("Bochum, ich komm' aus dir");
 {
     double x = 12345.6789;
 
-    Console.WriteLine($"|x: { x,15:plus 0.0;minus 0.0}|");
+    Console.WriteLine($"|x: {x,15:plus 0.0;minus 0.0}|");
     Console.WriteLine($"|x: {-x,15:plus 0.0;minus 0.0}|");
 }
 
-// Beispiele Format
-
+// Beispiele Format 1
+double var = 12345.6789;
 Console.WriteLine();
+Console.WriteLine($"{Math.PI}");
+Console.WriteLine($"{var,17}");
+Console.WriteLine($"{var,-17}");
+Console.WriteLine($"{Math.PI:0.00000}");
+Console.WriteLine($"{var:0.00000}");
 
+
+// Beispiele Format 2
 double force = 12345.6789;
+Console.WriteLine();
 Console.WriteLine($"| Kraft: {force} N        |");
 Console.WriteLine($"| Kraft: {force:0.00} N          |");
 Console.WriteLine($"| Kraft: {force,19:0.000} |");
