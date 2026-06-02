ef5ece5a38fb02a434b5d4ee612e217ccf37aeda
Matthias Baitsch <matthias.baitsch@me.com>
2026-06-02 17:50:58 +0200

    Add test project

---
+++ bausteine/06-kontrollstrukturen/aufgaben/projekt-test/01-fraction-test/FractionTest.cs
@@ -0,0 +1,13 @@
+﻿public class FractionTest
+{
+    [Fact]
+    public void TestSimplify()
+    {
+        Assert.Equal(new Fraction(1, 2), new Fraction(4, 8).Simplify());
+        Assert.Equal(new Fraction(2, 1), new Fraction(8, 4).Simplify());
+        Assert.Equal(new Fraction(0, 1), new Fraction(0, 33).Simplify());
+        Assert.Equal(new Fraction(1, 3), new Fraction(-1, -3).Simplify());
+        Assert.Equal(new Fraction(0, 1), new Fraction(0, 5).Simplify());
+        Assert.Equal(new Fraction(0, 1), new Fraction(0, -5).Simplify());
+    }
+}
---
+++ bausteine/06-kontrollstrukturen/aufgaben/projekt-test/01-fraction/Fraction.cs
@@ -0,0 +1,124 @@
+using static System.Math;
+
+public class Fraction
+{
+
+    public int A;
+    public int B;
+
+    public Fraction(int a, int b)
+    {
+        if (a * b >= 0)
+        {
+            this.A = Abs(a);
+            this.B = Abs(b);
+        }
+        else
+        {
+            this.A = -Abs(a);
+            this.B = Abs(b);
+        }
+    }
+
+    public void Print(string label)
+    {
+        Console.WriteLine($"{label} = {this.A} / {this.B}");
+    }
+
+    public Fraction Inverse()
+    {
+        return new Fraction(this.B, this.A);
+    }
+
+    public double ToDouble()
+    {
+        return (double)this.A / this.B;
+    }
+
+    public Fraction Add(Fraction q)
+    {
+        int a = this.A * q.B + this.B * q.A;
+        int b = this.B * q.B;
+
+        return new Fraction(a, b);
+    }
+
+    public Fraction Subtract(Fraction q)
+    {
+        int a = this.A * q.B - this.B * q.A;
+        int b = this.B * q.B;
+
+        return new Fraction(a, b);
+    }
+
+    public Fraction Multiply(Fraction q)
+    {
+        int a = this.A * q.A;
+        int b = this.B * q.B;
+
+        return new Fraction(a, b);
+    }
+
+    public Fraction Divide(Fraction q)
+    {
+        return this.Multiply(q.Inverse());
+    }
+
+    public int CompareTo(Fraction q)
+    {
+        int z1 = this.A * q.B;
+        int z2 = this.B * q.A;
+
+        if (z1 < z2)
+        {
+            return -1;
+        }
+        else if (z1 == z2)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    public bool Equals(Fraction q)
+    {
+        return this.A == q.A && this.B == q.B;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Fraction q)
+        {
+            return this.Equals(q);
+        }
+        return false;
+    }
+
+    public bool IsEquivalent(Fraction q)
+    {
+        return this.A * q.B == q.A * this.B;
+    }
+
+    public Fraction Simplify()
+    {
+        int p = Abs(this.A);
+        int q = Abs(this.B);
+        int r = p % q;
+
+        while (r != 0)
+        {
+            p = q;
+            q = r;
+            r = p % q;
+        }
+        return new Fraction(this.A / q, this.B / q);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(this.A, this.B);
+    }
+}
