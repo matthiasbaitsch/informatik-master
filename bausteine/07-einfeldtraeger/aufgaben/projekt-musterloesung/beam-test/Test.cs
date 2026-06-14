68a43420b3a60d5cefc132b668aca3b456cfbc48
Matthias Baitsch <matthias.baitsch@me.com>
2026-06-14 11:14:30 +0200

    Alle tests mit Toleranz

--- bausteine/07-einfeldtraeger/aufgaben/projekt-musterloesung/beam-test/Test.cs
+++ bausteine/07-einfeldtraeger/aufgaben/projekt-musterloesung/beam-test/Test.cs
@@ -1,4 +1,4 @@
-﻿public class Test
+public class Test
 {
     public const double L = 3.2;
     public const double Q = 1.3;
@@ -7,180 +7,180 @@
     public void MAFixedFixed()
     {
         Beam b = new Beam(L, "Fixed", "Fixed", Q);
-        Assert.Equal(-Q * L * L / 12, b.MA());
+        Assert.Equal(-Q * L * L / 12, b.MA(), 1e-14);
     }
 
     [Fact]
     public void MBFixedFixed()
     {
         Beam b = new Beam(L, "Fixed", "Fixed", Q);
-        Assert.Equal(-Q * L * L / 12, b.MB());
+        Assert.Equal(-Q * L * L / 12, b.MB(), 1e-14);
     }
 
     [Fact]
     public void MAFixedPinned()
     {
         Beam b = new Beam(L, "Fixed", "Pinned", Q);
-        Assert.Equal(-Q * L * L / 8, b.MA());
+        Assert.Equal(-Q * L * L / 8, b.MA(), 1e-14);
     }
 
     [Fact]
     public void MBFixedPinned()
     {
         Beam b = new Beam(L, "Fixed", "Pinned", Q);
-        Assert.Equal(0, b.MB());
+        Assert.Equal(0.0, b.MB(), 1e-14);
     }
 
     [Fact]
     public void MAFixedFree()
     {
         Beam b = new Beam(L, "Fixed", "Free", Q);
-        Assert.Equal(-Q * L * L / 2, b.MA());
+        Assert.Equal(-Q * L * L / 2, b.MA(), 1e-14);
     }
 
     [Fact]
     public void MBFixedFree()
     {
         Beam b = new Beam(L, "Fixed", "Free", Q);
-        Assert.Equal(0, b.MB());
+        Assert.Equal(0.0, b.MB(), 1e-14);
     }
 
     [Fact]
     public void MAPinnedFixed()
     {
         Beam b = new Beam(L, "Pinned", "Fixed", Q);
-        Assert.Equal(0, b.MA());
+        Assert.Equal(0.0, b.MA(), 1e-14);
     }
 
     [Fact]
     public void MBPinnedFixed()
     {
         Beam b = new Beam(L, "Pinned", "Fixed", Q);
-        Assert.Equal(-Q * L * L / 8, b.MB());
+        Assert.Equal(-Q * L * L / 8, b.MB(), 1e-14);
     }
 
     [Fact]
     public void MAPinnedPinned()
     {
         Beam b = new Beam(L, "Pinned", "Pinned", Q);
-        Assert.Equal(0, b.MA());
+        Assert.Equal(0.0, b.MA(), 1e-14);
     }
 
     [Fact]
     public void MBPinnedPinned()
     {
         Beam b = new Beam(L, "Pinned", "Pinned", Q);
-        Assert.Equal(0, b.MB());
+        Assert.Equal(0.0, b.MB(), 1e-14);
     }
 
     [Fact]
     public void MAFreeFixed()
     {
         Beam b = new Beam(L, "Free", "Fixed", Q);
-        Assert.Equal(0, b.MA());
+        Assert.Equal(0.0, b.MA(), 1e-14);
     }
 
     [Fact]
     public void MBFreeFixed()
     {
         Beam b = new Beam(L, "Free", "Fixed", Q);
-        Assert.Equal(-Q * L * L / 2, b.MB());
+        Assert.Equal(-Q * L * L / 2, b.MB(), 1e-14);
     }
 
     [Fact]
     public void TestMMaxFixedFixed()
     {
         Beam b = new Beam(L, "Fixed", "Fixed", Q);
-        Assert.Equal(Q * L * L / 12, b.MMax());
+        Assert.Equal(Q * L * L / 12, b.MMax(), 1e-14);
     }
 
     [Fact]
     public void TestMMaxFixedPinned()
     {
         Beam b = new Beam(L, "Fixed", "Pinned", Q);
-        Assert.Equal(Q * L * L / 8, b.MMax());
+        Assert.Equal(Q * L * L / 8, b.MMax(), 1e-14);
     }
 
     [Fact]
     public void TestMMaxFixedFree()
     {
         Beam b = new Beam(L, "Fixed", "Free", Q);
-        Assert.Equal(Q * L * L / 2, b.MMax());
+        Assert.Equal(Q * L * L / 2, b.MMax(), 1e-14);
     }
 
     [Fact]
     public void TestMMaxPinnedFixed()
     {
         Beam b = new Beam(L, "Pinned", "Fixed", Q);
-        Assert.Equal(Q * L * L / 8, b.MMax());
+        Assert.Equal(Q * L * L / 8, b.MMax(), 1e-14);
     }
 
     [Fact]
     public void TestMMaxPinnedPinned()
     {
         Beam b = new Beam(L, "Pinned", "Pinned", Q);
-        Assert.Equal(Q * L * L / 8, b.MMax());
+        Assert.Equal(Q * L * L / 8, b.MMax(), 1e-14);
     }
 
     [Fact]
     public void TestMMaxFreeFixed()
     {
         Beam b = new Beam(L, "Free", "Fixed", Q);
-        Assert.Equal(Q * L * L / 2, b.MMax());
+        Assert.Equal(Q * L * L / 2, b.MMax(), 1e-14);
     }
 
     [Fact]
     public void TestMomentFixedFixed()
     {
         Beam b = new Beam(L, "Fixed", "Fixed", Q);
-        Assert.Equal(b.MA(), b.M(0));
-        Assert.Equal(Q * L * L / 24, b.M(L / 2), 1e-12);
-        Assert.Equal(b.MB(), b.M(L));
+        Assert.Equal(b.MA(), b.M(0), 1e-14);
+        Assert.Equal(Q * L * L / 24, b.M(L / 2), 1e-14);
+        Assert.Equal(b.MB(), b.M(L), 1e-14);
     }
 
     [Fact]
     public void TestMomentFixedPinned()
     {
         Beam b = new Beam(L, "Fixed", "Pinned", Q);
-        Assert.Equal(b.MA(), b.M(0));
-        Assert.Equal(Q * L * L / 16, b.M(L / 2));
-        Assert.Equal(b.MB(), b.M(L));
+        Assert.Equal(b.MA(), b.M(0), 1e-14);
+        Assert.Equal(Q * L * L / 16, b.M(L / 2), 1e-14);
+        Assert.Equal(b.MB(), b.M(L), 1e-14);
     }
 
     [Fact]
     public void TestMomentFixedFree()
     {
         Beam b = new Beam(L, "Fixed", "Free", Q);
-        Assert.Equal(b.MA(), b.M(0));
-        Assert.Equal(-Q * L * L / 8, b.M(L / 2));
-        Assert.Equal(b.MB(), b.M(L));
+        Assert.Equal(b.MA(), b.M(0), 1e-14);
+        Assert.Equal(-Q * L * L / 8, b.M(L / 2), 1e-14);
+        Assert.Equal(b.MB(), b.M(L), 1e-14);
     }
 
     [Fact]
     public void TestMomentPinnedFixed()
     {
         Beam b = new Beam(L, "Pinned", "Fixed", Q);
-        Assert.Equal(b.MA(), b.M(0));
-        Assert.Equal(Q * L * L / 16, b.M(L / 2));
-        Assert.Equal(b.MB(), b.M(L));
+        Assert.Equal(b.MA(), b.M(0), 1e-14);
+        Assert.Equal(Q * L * L / 16, b.M(L / 2), 1e-14);
+        Assert.Equal(b.MB(), b.M(L), 1e-14);
     }
 
     [Fact]
     public void TestMomentPinnedPinned()
     {
         Beam b = new Beam(L, "Pinned", "Pinned", Q);
-        Assert.Equal(b.MA(), b.M(0));
-        Assert.Equal(Q * L * L / 8, b.M(L / 2));
-        Assert.Equal(b.MB(), b.M(L));
+        Assert.Equal(b.MA(), b.M(0), 1e-14);
+        Assert.Equal(Q * L * L / 8, b.M(L / 2), 1e-14);
+        Assert.Equal(b.MB(), b.M(L), 1e-14);
     }
 
     [Fact]
     public void TestMomentFreeFixed()
     {
         Beam b = new Beam(L, "Free", "Fixed", Q);
-        Assert.Equal(b.MA(), b.M(0));
-        Assert.Equal(-Q * L * L / 8, b.M(L / 2));
-        Assert.Equal(b.MB(), b.M(L));
+        Assert.Equal(b.MA(), b.M(0), 1e-14);
+        Assert.Equal(-Q * L * L / 8, b.M(L / 2), 1e-14);
+        Assert.Equal(b.MB(), b.M(L), 1e-14);
     }
 
 }
